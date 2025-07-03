673053f0e91252fdb619a21436030369dac01b79
Derek Francour <dfrancour@endor.ai>
2025-07-02 20:00:35 -0700

    Paginate GetAllProjects

--- endor_common/js/projects.js
+++ endor_common/js/projects.js
@@ -3,10 +3,10 @@ class Projects {
         this.client = client;
     }
 
-    async getAllProjects(namespace, options = {}) {
+    async getAllProjects(namespace, traverse = true, options = {}) {
         const defaultOptions = {
             'list_parameters.page_size': 500,
-            'list_parameters.traverse': true,
+            'list_parameters.traverse': traverse,
             'list_parameters.mask': 'uuid,meta.name,tenant_meta.namespace'
         };
 
@@ -15,14 +15,38 @@ class Projects {
             ...options
         };
 
-        return await this.client.request(
-            'GET',
-            `/namespaces/${namespace}/projects`,
-            params
-        );
+        // Initialize variables for pagination
+        let allProjects = [];
+        let pageToken = null;
+
+        do {
+            const currentParams = pageToken
+                ? { ...params, 'list_parameters.page_token': pageToken }
+                : params;
+
+            const response = await this.client.request(
+                'GET',
+                `/namespaces/${namespace}/projects`,
+                currentParams
+            );
+
+            const projects = response.list?.objects || [];
+            allProjects = allProjects.concat(projects);
+
+            pageToken = response.list?.response?.next_page_token;
+        } while (pageToken);
+
+        return {
+            list: {
+                objects: allProjects,
+                response: {
+                    next_page_token: null
+                }
+            }
+        };
     }
 
     // Add other project-related methods here
 }
 
-module.exports = Projects; 
+module.exports = Projects;
